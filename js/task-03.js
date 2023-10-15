c2cc61a88a4147217c21e414bd949e3e5310d352
Andrew <asaljakin@gmail.com>
2023-10-15 19:01:37 +0300

    update task-3

--- js/task-03.js
+++ js/task-03.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', handlerSubmit);
 
+function handlerSubmit(event) {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+
+    if (!email.value || !password.value) {
+        alert('All fields must be filled!');
+        return;
+    }
+
+    const result = {
+        email: email.value,
+        password: password.value,
+    }
+
+    console.log(result);
+    event.currentTarget.reset();
+}
